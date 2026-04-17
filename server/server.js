9c10f1d7dd8d52844255fe12f4428a1000e5aae5
Hamza <hamza.junaid2002@gmail.com>
2026-04-17 19:33:57 +0530

    added frontend URL to CORS

--- server/server.js
+++ server/server.js
@@ -24,10 +24,9 @@ connectDB();
 
 app.use(cors({
   origin: [
-    "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://mern-event-platform-react.onrender.com"
+    "https://event-viewer-platform.netlify.app"
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
